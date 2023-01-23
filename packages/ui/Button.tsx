68bd39d143cce770354bc7d8b2fc5cbe6594649d
Idan Goor <idangoor1@gmail.com>
2023-01-23 13:08:00 +0200

    add log

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,15 +1,17 @@
-import * as React from "react";
-import { sayHello } from "utils";
+import * as React from "react"
+import { sayHello } from "utils"
 
 export interface ButtonProps {
-  onClick?: () => void;
+  onClick?: () => void
 }
 
 export const Button = ({ onClick }: ButtonProps) => {
   const handleClick = () => {
-    sayHello("Idan");
-    onClick?.();
-  };
+    sayHello("Idan")
+    onClick?.()
 
-  return <button onClick={handleClick}>Boop</button>;
-};
+    console.log("boom")
+  }
+
+  return <button onClick={handleClick}>Boop</button>
+}
