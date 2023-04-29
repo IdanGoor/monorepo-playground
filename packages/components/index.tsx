a71d71dcaed5fe35019092f116e7f4eda0892e34
Idan Goor <idangoor1@gmail.com>
2023-04-29 22:10:36 +0300

    add changes

--- packages/components/index.tsx
+++ packages/components/index.tsx
@@ -1,11 +1,19 @@
-import * as React from "react";
-import { Button } from "@monorepo-playground/ui";
-import { sayHello } from "utils";
+import * as React from "react"
+import { Button, Input } from "@monorepo-playground/ui"
+import { sayHello } from "utils"
 
 export const SpecialButton = () => {
-  return <Button onClick={() => sayHello("Ofer")} />;
-};
+  return <Button onClick={() => sayHello("Ofer")} />
+}
 
 export const MagicButton = () => {
-  return <Button onClick={() => sayHello("Magic!")} />;
-};
+  return <Button onClick={() => sayHello("Magic!")} />
+}
+
+export const Page = () => {
+  return (
+    <div>
+      <Input />
+    </div>
+  )
+}
